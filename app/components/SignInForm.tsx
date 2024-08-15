064c454a06de53e6880820bcd30725d8f9744161
Anton Lovrić <antonlovri81@gmail.com>
2024-08-15 23:34:39 +0200

    fix(auth): changed auth loading states

--- app/components/SignInForm.tsx
+++ app/components/SignInForm.tsx
@@ -42,34 +42,32 @@ const SignInForm = ({ handleSignIn, errorMessage }: ISignInForm) => {
   }
 
   return (
-    <LoadingWrapper isLoading={isLoading}>
-      <form action={submitUser} className="flex flex-col gap-4">
-        <div className="flex flex-col gap-1 align-center">
-          <label htmlFor="email">Email</label>
-          <input
-            onChange={handleUpdateForm}
-            type="email"
-            id="email"
-            value={formValues.email}
-            className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
-          />
-        </div>
-        <div className="flex flex-col gap-1 align-center">
-          <label htmlFor="password">Password</label>
-          <input
-            onChange={handleUpdateForm}
-            type="password"
-            id="password"
-            value={formValues.password}
-            className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
-          />
-        </div>
-        {errorMessage ? 'Invalid credentials' : ''}
-        <button type="submit" className="button-primary mt-2">
-          Sign In
-        </button>
-      </form>
-    </LoadingWrapper>
+    <form action={submitUser} className="flex flex-col gap-4">
+      <div className="flex flex-col gap-1 align-center">
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={handleUpdateForm}
+          type="email"
+          id="email"
+          value={formValues.email}
+          className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
+        />
+      </div>
+      <div className="flex flex-col gap-1 align-center">
+        <label htmlFor="password">Password</label>
+        <input
+          onChange={handleUpdateForm}
+          type="password"
+          id="password"
+          value={formValues.password}
+          className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
+        />
+      </div>
+      {errorMessage ? 'Invalid credentials' : ''}
+      <button type="submit" className="button-primary mt-2">
+        <LoadingWrapper isLoading={isLoading}>Sign In</LoadingWrapper>
+      </button>
+    </form>
   );
 };
 
--- app/components/SignUpForm.tsx
+++ app/components/SignUpForm.tsx
@@ -49,53 +49,51 @@ const SignUpForm = ({ handleSignUp }: ISignUpForm) => {
   }
 
   return (
-    <LoadingWrapper isLoading={isLoading}>
-      <form action={submitUser} className="flex flex-col gap-4">
-        <div className="flex flex-col gap-1 align-center">
-          <label htmlFor="firstName">First name</label>
-          <input
-            onChange={handleUpdateForm}
-            type="text"
-            id="firstName"
-            value={formValues.firstName}
-            className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
-          />
-        </div>
-        <div className="flex flex-col gap-1 align-center">
-          <label htmlFor="lastName">Last name</label>
-          <input
-            onChange={handleUpdateForm}
-            type="text"
-            id="lastName"
-            value={formValues.lastName}
-            className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
-          />
-        </div>
-        <div className="flex flex-col gap-1 align-center">
-          <label htmlFor="email">Email</label>
-          <input
-            onChange={handleUpdateForm}
-            type="email"
-            id="email"
-            value={formValues.email}
-            className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
-          />
-        </div>
-        <div className="flex flex-col gap-1 align-center">
-          <label htmlFor="password">Password</label>
-          <input
-            onChange={handleUpdateForm}
-            type="password"
-            id="password"
-            value={formValues.password}
-            className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
-          />
-        </div>
-        <button type="submit" className="button-primary mt-2">
-          Sign Up
-        </button>
-      </form>
-    </LoadingWrapper>
+    <form action={submitUser} className="flex flex-col gap-4">
+      <div className="flex flex-col gap-1 align-center">
+        <label htmlFor="firstName">First name</label>
+        <input
+          onChange={handleUpdateForm}
+          type="text"
+          id="firstName"
+          value={formValues.firstName}
+          className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
+        />
+      </div>
+      <div className="flex flex-col gap-1 align-center">
+        <label htmlFor="lastName">Last name</label>
+        <input
+          onChange={handleUpdateForm}
+          type="text"
+          id="lastName"
+          value={formValues.lastName}
+          className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
+        />
+      </div>
+      <div className="flex flex-col gap-1 align-center">
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={handleUpdateForm}
+          type="email"
+          id="email"
+          value={formValues.email}
+          className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
+        />
+      </div>
+      <div className="flex flex-col gap-1 align-center">
+        <label htmlFor="password">Password</label>
+        <input
+          onChange={handleUpdateForm}
+          type="password"
+          id="password"
+          value={formValues.password}
+          className="text-white bg-dark-gray rounded-md py-2 px-4 w-full border border-light-gray"
+        />
+      </div>
+      <button type="submit" className="button-primary mt-2">
+        <LoadingWrapper isLoading={isLoading}>Sign Up</LoadingWrapper>
+      </button>
+    </form>
   );
 };
 
