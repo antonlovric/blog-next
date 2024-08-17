3fe56c78539405e61ced78582fd24037ae243c06
Anton Lovrić <antonlovri81@gmail.com>
2024-08-17 20:39:39 +0200

    fix(home): post cards are now displayed in a grid and only the latest 6 posts are shown

--- app/(authenticated-pages)/page.tsx
+++ app/(authenticated-pages)/page.tsx
@@ -46,9 +46,12 @@ export default async function Home() {
       },
     },
   });
-  const filteredPostCategories = postsByCategory.filter(
-    (category) => category.post_categories.length
-  );
+  const filteredPostCategories = postsByCategory
+    .filter((category) => category.post_categories.length)
+    .map((category) => ({
+      ...category,
+      post_categories: category.post_categories.slice(0, 5),
+    }));
 
   return (
     <div className={inter.className}>
@@ -68,17 +71,17 @@ export default async function Home() {
               <button className="button-primary">More</button>
             </Link>
           </div>
-          <div className="flex items-start gap-8 w-full overflow-x-scroll">
+          <div className="grid md:grid-cols-5 sm:grid-cols-2 grid-cols-1 gap-y-8 gap-x-6 overflow-x-scroll mt-2">
             {category.post_categories.map((post) => (
               <Link
                 href={`post/${post.posts_id}`}
-                className="bg-dark-gray rounded-md p-2 cursor-pointer relative max-w-[350px]"
+                className="bg-dark-gray  rounded-md p-2 cursor-pointer h-full flex flex-col justify-between relative"
                 key={`${post.categories_id}-${post.posts_id}`}
               >
                 <img
                   src={post.posts.cover_image || ''}
                   alt=""
-                  className=" h-[250px] w-full object-cover"
+                  className="block rounded-md h-[250px] w-full object-cover"
                 />
                 <span
                   dangerouslySetInnerHTML={{
