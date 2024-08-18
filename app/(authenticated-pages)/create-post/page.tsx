2fa583070dc18034e96d67fb05044d9851a3dba0
Anton Lovrić <antonlovri81@gmail.com>
2024-08-18 21:49:01 +0200

    chore(misc): removed unused function

--- app/(authenticated-pages)/create-post/page.tsx
+++ app/(authenticated-pages)/create-post/page.tsx
@@ -1,7 +1,5 @@
 import TextEditor from '@/app/components/TextEditor';
 import { prisma } from '@/app/helpers/api';
-import { getActiveUser } from '@/app/helpers/auth';
-import { revalidatePath } from 'next/cache';
 import React from 'react';
 export interface ICreatePostRequest {
   html_content: string;
@@ -17,40 +15,6 @@ export interface IImageUploadResponse {
 
 const CreatePost = async () => {
   const categories = await prisma.categories.findMany();
-
-  async function handleCreate(props: ICreatePostRequest) {
-    'use server';
-
-    try {
-      const activeUser = getActiveUser();
-      if (activeUser?.id) {
-        const res = await prisma.posts.create({
-          data: {
-            html_content: props.html_content,
-            summary: props.summary,
-            title: props.title,
-            post_categories: {
-              create: props.categoryIds.map((categoryId) => ({
-                categories: {
-                  connect: {
-                    id: categoryId,
-                  },
-                },
-              })),
-            },
-            users_id: activeUser.id,
-            cover_image: props.coverImagePath,
-          },
-        });
-      }
-      revalidatePath('/');
-      revalidatePath('/posts');
-    } catch (error) {
-      console.error('ERROR CREATING POST');
-      console.error(error);
-      throw error;
-    }
-  }
 
   return (
     <div>
