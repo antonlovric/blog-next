39153a216a96c896dd75b9915af4b69f06a69e17
Anton Lovrić <antonlovri81@gmail.com>
2024-08-27 21:29:07 +0200

    fixed featured post render issue

--- app/components/FeaturedPost.tsx
+++ app/components/FeaturedPost.tsx
@@ -51,53 +51,52 @@ const FeaturedPost = async () => {
   const sanitizedSummary = DOMPurify.sanitize(post.summary || '');
 
   return (
-    <Link
-      className="grid grid-cols-2 gap-x-8 w-5/6 mx-auto mt-10"
-      href={`/post/${post.id}`}
-    >
-      <Image
-        alt="Blog image"
-        src={post.cover_image || ''}
-        height={500}
-        width={750}
-        className="border-2 border-blog-blue rounded-md h-[400px] w-[650px] object-cover"
-      />
-      <div className="flex flex-col justify-between">
-        <div className="flex flex-col gap-3">
-          <div className="flex items-center gap-3">
-            {post.post_categories.map((category) => (
-              <span
-                key={category.categories.id}
-                className="bg-blog-blue px-2 py-1 rounded-md"
-              >
-                {category.categories.name}
-              </span>
-            ))}
-          </div>
-          <div>
-            <div
-              className="text-subheading font-semibold"
-              dangerouslySetInnerHTML={{ __html: sanitizedTitle }}
-            ></div>
-            <div
-              className="font-light text-xl text-justify mt-4"
-              dangerouslySetInnerHTML={{ __html: sanitizedSummary }}
-            ></div>
-          </div>
-        </div>
-        <div className="flex font-extralight text-lg justify-between items-center">
-          <div className="flex items-center justify-between gap-2">
-            <div className="h-[40px] w-[40px]">
-              <ProfileImage imagePath={post.author.profile_image} />
+    <Link href={`/post/${post.id}`}>
+      <article className="grid grid-cols-2 gap-x-8 w-5/6 mx-auto mt-10">
+        <Image
+          alt="Blog image"
+          src={post.cover_image || ''}
+          height={500}
+          width={750}
+          className="border-2 border-blog-blue rounded-md h-[400px] w-[650px] object-cover"
+        />
+        <div className="flex flex-col justify-between">
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center gap-3">
+              {post.post_categories.map((category) => (
+                <span
+                  key={category.categories.id}
+                  className="bg-blog-blue px-2 py-1 rounded-md"
+                >
+                  {category.categories.name}
+                </span>
+              ))}
             </div>
-
-            <p>
-              by {post.author.first_name} {post.author.last_name}
-            </p>
+            <div>
+              <div
+                className="text-subheading font-semibold"
+                dangerouslySetInnerHTML={{ __html: sanitizedTitle }}
+              ></div>
+              <div
+                className="font-light text-xl text-justify mt-4"
+                dangerouslySetInnerHTML={{ __html: sanitizedSummary }}
+              ></div>
+            </div>
           </div>
-          <p> {formatDate(post.created_at)}</p>
+          <div className="flex font-extralight text-lg justify-between items-center">
+            <div className="flex items-center justify-between gap-2">
+              <div className="h-[40px] w-[40px]">
+                <ProfileImage imagePath={post.author.profile_image} />
+              </div>
+
+              <p>
+                by {post.author.first_name} {post.author.last_name}
+              </p>
+            </div>
+            <p> {formatDate(post.created_at)}</p>
+          </div>
         </div>
-      </div>
+      </article>
     </Link>
   );
 };
